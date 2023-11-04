15089ed24133be0d551679a24f250443b1727af9
AdamKing402 <118691521+AdamKing402@users.noreply.github.com>
2023-11-04 16:49:39 -0400

    New Beeware Location Test/Basic UI
    This adds the Beeware library to a new location to determine if launching the project is easier. Also contains the start for a basic UI.

--- BSafe_App/beeware-venv/Scripts/activate.bat
+++ BSafe_App/beeware-venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rmann\OneDrive\Desktop\Bsafe\beeware-venv
+set VIRTUAL_ENV=C:\Users\adams\OneDrive\Desktop\BSafe\BSafeHelmet\BSafe_App\beeware-venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,7 +25,6 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(beeware-venv) 
 
 :END
 if defined _OLD_CODEPAGE (
--- BSafe_App/bsafe/src/bsafe/app.py
+++ BSafe_App/bsafe/src/bsafe/app.py
@@ -9,15 +9,30 @@ from toga.style.pack import COLUMN, ROW
 class BSafe(toga.App):
 
     def startup(self):
-        """
-        Construct and show the Toga application.
+        main_box = toga.Box(style=Pack(direction=COLUMN, padding=20, alignment="center"))
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        # Create a label to display GPS location
+        gps_label = toga.Label('GPS Location:', style=Pack(padding=(0, 10), alignment="center"))
+        gps_location = toga.Label('Waiting for location...', style=Pack(padding=(0, 10), alignment="center"))
 
+        # Create a label to display Bluetooth connection status
+        bluetooth_label = toga.Label('Bluetooth Status:', style=Pack(padding=(0, 10), alignment="center"))
+        bluetooth_status = toga.Label('Not connected', style=Pack(padding=(0, 10), alignment="center"))
+
+        # Create a square box for GPS location (you can customize size and appearance)
+        square_box = toga.Box(style=Pack(direction=ROW, alignment="center"))
+        square_box.add(gps_location)
+
+        # Center the labels and square box horizontally
+        main_box.add(gps_label)
+        main_box.add(square_box)
+        main_box.add(bluetooth_label)
+        main_box.add(bluetooth_status)
+
+        # Align the components to the left side of the main box
+        # main_box.alignment = LEFT
+
+        # Set the main box as the main content of the main window
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
